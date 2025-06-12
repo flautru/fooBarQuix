87497c0f493c6511a25c792034ad28dab05eff26
flautru <flautru@gmail.com>
2025-06-12 15:00:55 +0200

    update FooBarQuixService
    Reformatage FooBarQuixService

--- src/main/java/com/fabien/kata/fooBarQuix/service/FooBarQuixService.java
+++ src/main/java/com/fabien/kata/fooBarQuix/service/FooBarQuixService.java
@@ -10,18 +10,26 @@ public class FooBarQuixService {
     public String getFooBarQuix (Integer num ) {
         validateNum(num);
         StringBuilder sRet = new StringBuilder();
-        // Dans ce cas 0 est considéré comme divisible par 3 et 5
-        if(num % 3 == 0) sRet.append(FOO);
-        if(num % 5 == 0) sRet.append(BAR);
 
+        if(num % 3 == 0) {
+            sRet.append(FOO);
+        }
+        if(num % 5 == 0){
+            sRet.append(BAR);
+        }
+
+        //Verification de gauche à droite si num contient un 3,5,7
         String strNum = String.valueOf(num);
         strNum.chars().forEach( c -> {
             if(c == '3' ) sRet.append(FOO);
             if(c == '5') sRet.append(BAR);
             if(c == '7') sRet.append(QUIX);
-                });
+        });
 
-        if(sRet.toString().isEmpty()) sRet.append(num);
+        // Si sRet est vide alors aucune règle n'est vérifié on retourne le num
+        if(sRet.toString().isEmpty()) {
+            sRet.append(num);
+        }
 
         return sRet.toString();
     }
